0e5691cac3bca92940ad8ad6b48f9527d1005710
ar <ar@ar-inspiron-7460>
2021-06-08 19:41:24 -0300

    Implementação do classe Graph

---
+++ src/mestrado/Graph.java
@@ -0,0 +1,58 @@
+package mestrado;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Graph<T> {
+
+    private List<Vertex<T>> vertexs = new ArrayList<>();
+
+    private final int[][] adjacencyMatrix;
+
+    public Graph() {
+        adjacencyMatrix = new int[100][100];
+
+        /**
+         * Haja vista que o grafo é inicialmente vazio, a matrix de adjacência é inicializada com 0's
+         */
+        for (int i = 0; i < adjacencyMatrix.length; i++) {
+            for (int j = 0; j < adjacencyMatrix[i].length; j++) {
+                adjacencyMatrix[i][j] = 0;
+            }
+        }
+    }
+    
+    public void addVertex(T label) {
+        vertexs.add(new Vertex<T>(label));
+    }
+    
+    public void bind(T label01, T label02) {
+        Integer index01 = vertexs.indexOf(new Vertex<T>(label01));
+        Integer index02 = vertexs.indexOf(new Vertex<T>(label02));
+        
+        adjacencyMatrix[index01][index02] = 1;
+        adjacencyMatrix[index02][index01] = 1;
+    }
+    
+    public List<Vertex<T>> getVertexs() {
+        return this.vertexs;
+    }
+    
+    public List<Vertex<T>> getNeighbors(T label) {
+        Integer index = getVertexs().indexOf(new Vertex<T>(label));
+        
+        List<Vertex<T>> neighbors = new ArrayList<>();
+        for (int i = 0; i < getAdjacencyMatrix()[index].length; i++) {
+            if (getAdjacencyMatrix()[index][i] == 1) {
+                neighbors.add(getVertexs().get(i));
+            }
+        }
+        
+        return neighbors;
+    }
+
+    public int[][] getAdjacencyMatrix() {
+        return adjacencyMatrix;
+    }
+
+}
